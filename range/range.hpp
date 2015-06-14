a82790e2354aba88e2179134ffe6998169026f89
cielavenir <cielartisan@gmail.com>
2015-06-15 02:30:30 +0900

    update range

--- range/range.hpp
+++ range/range.hpp
@@ -1,12 +1,17 @@
-// cpp_range (generic range class) by @cielavenir under Boost Software License.
-// note: start and end positions are inclusive.
+// cpp_range (generic range class, very small equivalent of boost::irange)
+// (C) @cielavenir under Boost Software License.
+// note: end position is exclusive.
 
+#include <iterator>
 template<typename T>
 class range{
 public:
 	struct iterator{
-		T a,b,p;
-		int d;
+		const T a,b;
+		T p;
+		const long long d; //because T might be unsigned.
+		iterator(T _a,T _b,T _p,long long _d=1):a(_a),b(_b),p(_p),d(_d){}
+
 	public:
 		typedef T value_type;
 		typedef T& reference;
@@ -14,42 +19,48 @@ public:
 		typedef std::ptrdiff_t difference_type;
 		typedef std::random_access_iterator_tag iterator_category;
 
-		iterator(T _a,T _b):a(_a),b(_b),p(_a),d(1){}
-		iterator(T _a,T _b,T _p,int _d=1):a(_a),b(_b),p(_p),d(_d){}
-		void operator=(const T &other){a=other.a,b=other.b,p=other.p,d=other.d;}
+		//copy
+		iterator(const iterator &other):a(other.a),b(other.b),p(other.p),d(other.d){}
+		iterator operator=(const iterator &other){return iterator(other.a,other.b,other.p,other.d);}
 
 		//advance
 		iterator& operator+=(T n){p+=n*d;return *this;}
 		iterator& operator-=(T n){return *this+=(-n);}
 		iterator& operator++(){return *this+=1;}
 		iterator& operator--(){return *this-=1;}
-		iterator operator+(T n){return iterator(a,b,p+n*d);}
-		iterator operator-(T n){return *this+(-n);}
+		iterator operator+(T n) const{return iterator(a,b,p+n*d);}
+		iterator operator-(T n) const{return *this+(-n);}
 
 		//difference
-		T operator-(const iterator& other){return p-other.p;}
+		T operator-(const iterator& other) const{return p-other.p;}
 
 		//equality
 		bool operator==(const iterator& other) const{return a==other.a && b==other.b && d==other.d && p==other.p;}
 		bool operator!=(const iterator& other) const{return !(*this==other);}
 
 		//compare
-		bool operator<(const iterator& other) const{return a==other.a && b==other.b && d==other.d && p<other.p;}
-		bool operator>(const iterator& other) const{return a==other.a && b==other.b && d==other.d && p>other.p;}
+		bool operator<(const iterator& other) const{return a==other.a && b==other.b && d==other.d && p*d<other.p*d;}
+		bool operator>(const iterator& other) const{return a==other.a && b==other.b && d==other.d && p*d>other.p*d;}
 
 		//reference
-		const T& operator*(){return p;}
+		const T& operator*() const{return p;}
 	};
 
 private:
-	T a,b;
+	const T a,b;
+	T siz;
+	long long d;
 public:
-	range(T _a,T _b):a(_a),b(_b){}
+	range(T _a,T _b,long long _d=1):a(_a),b(_b){
+		d=_d;
+		if(d==0)d=1; //
+		siz=( (d>0?(b-a):(a-b)) - 1)  / (d>0?d:-d);
+	}
 
-	T operator[](T n){return a-n;}
-	iterator begin(){return iterator(a,b);}
-	iterator end(){return iterator(a,b,b+1,1);}
-	iterator rbegin(){return iterator(a,b,b,-1);}
-	iterator rend(){return iterator(a,b,a-1,-1);}
-	T size(){return b-a+1;}
+	T operator[](T n){return a+n*d;}
+	iterator begin(){return iterator(a,a+d*siz,a,d);}
+	iterator end(){return iterator(a,a+d*siz,a+d*(siz+1),d);}
+	iterator rbegin(){return iterator(b-d*siz,b,b,-1*d);}
+	iterator rend(){return iterator(b-d*siz,b,b-d*(siz+1),-1*d);}
+	T size(){return siz;}
 };
