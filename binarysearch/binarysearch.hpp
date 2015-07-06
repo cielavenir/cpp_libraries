c76598224cbaf65e592bd5b6cc56822b2e1943aa
cielavenir <cielartisan@gmail.com>
2015-07-06 18:06:37 +0900

    modified binarysearch interface

--- binarysearch/binarysearch.hpp
+++ binarysearch/binarysearch.hpp
@@ -2,7 +2,7 @@
 // type F should be something like std::function<bool(T)>.
 
 template<typename T,typename F>
-T binarysearch(T lo,T hi,F checker,T eps=1){
+T binarysearch(T lo,T hi,T eps,const F &checker){
 	for(;lo+eps<hi;){
 		T mi=(lo+hi)/2;
 		if(checker(mi)){
@@ -13,3 +13,5 @@ T binarysearch(T lo,T hi,F checker,T eps=1){
 	}
 	return lo;
 }
+template<typename T,typename F>
+T binarysearch(T lo,T hi,const F &checker){return binarysearch(lo,hi,1,checker);}
