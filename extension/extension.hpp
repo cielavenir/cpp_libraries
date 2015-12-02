a9d453f6688b81916a86a57dbfe1bc530caac415
cielavenir <cielartisan@gmail.com>
2015-12-03 00:59:29 +0900

    ported hasher from boost

--- extension/extension.hpp
+++ extension/extension.hpp
@@ -1,6 +1,10 @@
 // stdc++ extension
 // (C) @cielavenir under Boost Software License.
 
+#include <cstddef>
+
+#include <bitset>
+
 /// bitset comparator
 namespace std{
 	template<size_t N>
@@ -18,12 +22,110 @@ namespace std{
 }
 
 #if __cplusplus>=201100
-/// pair<int,int> hasher
+#include <array>
+#include <vector>
+#include <deque>
+#include <list>
+#include <forward_list>
+#include <set>
+#include <map>
+
+/// hasher
 namespace std{
+	template<typename T>
+	inline void hash_combine(size_t& seed, T const& v){
+		seed ^= hash<T>()(v) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+	}
+    template<typename It>
+    inline size_t hash_range(It first, It last){
+		size_t seed=0;
+		hash_range(seed,first,last);
+		return seed;
+	}
+	template<typename It>
+	inline void hash_range(size_t& seed, It first, It last){
+		for(;first!=last;++first)hash_combine(seed, *first);
+	}
+	template<typename A,typename B>
+	class hash<pair<A,B>>{
+		public:
+		size_t operator()(pair<A,B> const &p) const{
+			size_t seed=0;
+			hash_combine(seed,p.first);
+			hash_combine(seed,p.second);
+			return seed;
+		}
+	};
+	template<typename T,size_t N>
+	class hash<array<T,N>>{
+		public:
+		size_t operator()(array<T,N> const &container) const{
+			return hash_range(container.begin(),container.end());
+		}
+	};
+	template<typename T,typename A>
+	class hash<vector<T,A>>{
+		public:
+		size_t operator()(vector<T,A> const &container) const{
+			return hash_range(container.begin(),container.end());
+		}
+	};
+	template<typename T,typename A>
+	class hash<deque<T,A>>{
+		public:
+		size_t operator()(deque<T,A> const &container) const{
+			return hash_range(container.begin(),container.end());
+		}
+	};
+	template<typename T,typename A>
+	class hash<list<T,A>>{
+		public:
+		size_t operator()(list<T,A> const &container) const{
+			return hash_range(container.begin(),container.end());
+		}
+	};
+	template<typename T,typename A>
+	class hash<forward_list<T,A>>{
+		public:
+		size_t operator()(forward_list<T,A> const &container) const{
+			return hash_range(container.begin(),container.end());
+		}
+	};
+	template<typename T,typename C,typename A>
+	class hash<set<T,C,A>>{
+		public:
+		size_t operator()(set<T,C,A> const &container) const{
+			return hash_range(container.begin(),container.end());
+		}
+	};
+	template<typename T,typename C,typename A>
+	class hash<multiset<T,C,A>>{
+		public:
+		size_t operator()(multiset<T,C,A> const &container) const{
+			return hash_range(container.begin(),container.end());
+		}
+	};
+	template<typename K,typename T,typename C,typename A>
+	class hash<map<K,T,C,A>>{
+		public:
+		size_t operator()(map<K,T,C,A> const &container) const{
+			return hash_range(container.begin(),container.end());
+		}
+	};
+	template<typename K,typename T,typename C,typename A>
+	class hash<multimap<K,T,C,A>>{
+		public:
+		size_t operator()(multimap<K,T,C,A> const &container) const{
+			return hash_range(container.begin(),container.end());
+		}
+	};
+}
+
+/*
 	template<typename I>
 	class hash<pair<I,I>>{
 		public:
-		size_t operator()(const pair<I,I> &p) const {return hash<long long>() (((long long)(p.first))<<32|p.second);}
+		size_t operator()(pair<I,I> const &p) const {return hash<long long>() (((long long)(p.first))<<32|p.second);}
 	};
-}
+*/
 #endif
